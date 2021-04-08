ebc5c3b8248448996f53f230b1d8bd7c7c97450c
shrey <shreyshukladm@gmail.com>
2021-04-08 21:57:13 +0530

    hand input with animation
    hand input with animation, trigger and grip is used for mapping the action

---
+++ Assets/Scripts/Handpresence.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Handpresence : MonoBehaviour
+{
+    public bool showcontroller = false;
+    public InputDeviceCharacteristics controllercharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handmodelprefab;
+
+    private InputDevice targetdevice;
+    private GameObject spawnedcontroller;
+    private GameObject spawnedhandmodel;
+    private Animator handanimator;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllercharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetdevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetdevice.name);
+            if (prefab)
+            {
+                spawnedcontroller = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.Log("did not find the correct controller");
+                spawnedcontroller = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            spawnedhandmodel = Instantiate(handmodelprefab, transform);
+            handanimator = spawnedhandmodel.GetComponent<Animator>();
+        }
+    }
+
+    private void UpdateHandAnimator()
+    {
+        if (targetdevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handanimator.SetFloat("trigger", triggerValue);
+        }
+        else
+        {
+            handanimator.SetFloat("trigger", 0);
+        }
+
+        if (targetdevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handanimator.SetFloat("grip", gripValue);
+        }
+        else
+        {
+            handanimator.SetFloat("grip", 0);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (showcontroller)
+        {
+            spawnedhandmodel.SetActive(false);
+            spawnedcontroller.SetActive(true);
+        }
+        else
+        {
+            spawnedhandmodel.SetActive(true);
+            spawnedcontroller.SetActive(false);
+            UpdateHandAnimator();
+        }
+    }
+}
